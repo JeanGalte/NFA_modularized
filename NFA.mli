571afd225004355e2db7593ef2f4214e366db073
JeanGalte <102584320+JeanGalte@users.noreply.github.com>
2023-05-06 12:30:42 +0200

    Update NFA.mli
    Remodelage quasi total pour supporter les langages, y compris produits.

--- NFA.mli
+++ NFA.mli
@@ -1,5 +1,7 @@
 open Set
 
+(* /!\ Ce n'est pas une implémentation d'automate fini non déterministe classique, voir le README pour plus d'informations *)
+
 module type MonoidalType =
 (*
 Comme OrderedType mais assure en plus la structure de monoïde d'un type, ça permet de faire notre alphabet.
@@ -7,40 +9,77 @@ Assure la possibilité de faire un monoïde libre et semi groupe libre sur un en
 On laissera libre l'implémentation des habitants de MonoidalType
 *)
 sig
-	type t
+(* Usuellement string *)
+	type t 
+(* Usuellement > *)
 	val compare : t -> t -> int
+(* Usuellement ^ *)
 	val ( + ) : t -> t -> t
+(* Théoriquement epsilon, on prendra usuellement "e" puisqu'on est en ascii *)
 	val e : t
 end
+
+module type Monset =
+sig
+	include MonoidalType
+	module S : Set.S with type elt = t
+end 
+
+module type MonSetMakeT = functor (MT : MonoidalType) -> Monset 
+
+module MonsetMake : MonSetMakeT
+
+module type Lang =
+sig
+	module Sub : Monset
+(* Attention, Sub.t c'est Monset.t c'est un mot, Sub.S.t c'est un ensemble de mots Set.S.t *)
+	type t =
+		| B of Sub.S.t
+		| Star of Sub.t
+		| Union of t * t
+		| Prod of t * t
+	val compare : t -> t -> int
+end
+
+module LangMake (M : Monset) : Lang 
+
+(* Décrit les tages étiquetant les transitions d'un automate, voir le document en readme pour bien comprendre pourquoi c'est nécessaire *)
+(*	L'implémentation de * est laissée libre, mais à ma connaissance la seule implémentation pertinente qui colle à la théorie se trouve dans le .ml, si vous en connaissez une autre n'hésitez pas à m'en faire part *)
+module type SetLang = 
+sig
+	include Lang
+	module S : Set.S with type elt = t
+end
+
+module type SetLangMakeT = functor (L : Lang) -> SetLang 
+
+module SetLangMake : SetLangMakeT
 
 module type EtatalType =
 sig
 	type t
 	val compare : t -> t -> int	
-	(* Valeur de début et fin réservées (on en a besoin pour trouver le langage accepté lors quand on procède par élimination d'états) *)
 	val d : t
 	val f : t
-	val est_debut : t -> bool
-	val est_fin : t -> bool
 end
 
-module type MonSet =
+module type SetEtat = 
 sig
-	type x
-	include Set.S with type elt = x
-	val ( + ) : x -> x -> x
-	val e : x
+	include EtatalType
+	module S : Set.S with type elt = t
 end
 
-module type EtSet =
-sig
-	type x
-	include Set.S with type elt = x
-	val d : x
-	val f : x
-	val est_debut : x -> bool
-	val est_fin : x -> bool
+module type SetEtatMakeT = functor (E : EtatalType) -> SetEtat
+
+module SetEtatMake : SetEtatMakeT
+(*
+Enforce pratiquement totalement la structure du code, SetEtat implémentera au moins
+struct
+	include E
+	include Set.Make(E)
 end
+Et c'est tout ce dont on a besoin
+*)
 
 module type Aut = 
 sig
@@ -62,39 +101,14 @@ sig
 	val from_lists : a list -> q list -> q list -> q list -> (q * a * q) list -> aut  
 	val is_auto : auta -> autq -> autq -> autq -> autt -> bool  
 	val is_auto_from_aut : aut -> bool
-	val is_auto_from_lists : a list -> q list -> q list -> q list -> (q * a * q) list -> bool
-	val debuts_fins_uniques : aut -> aut
-	val est_chemin : aut -> a list  -> q -> q -> bool
-	(* Teste si un chemin mène d'un état à un autre*)
-	val accepte : aut -> a list -> bool
+	val is_auto_from_lists : a list -> q list -> q list -> q list -> (q * a * q) list -> bool  
+	val debuts_fins_uniques : aut -> aut 	
+	(* Vérifie si un automate accepte un langage *)
 end
 
-module type MonoidalTypeMake = functor (T : OrderedType) -> MonoidalType with type t = T.t
-
-module type EtatalTypeMake = functor (T : OrderedType) -> EtatalType with type t = T.t
-
-module type MonSetMake = functor (MT : MonoidalType) -> 
-	MonSet with type x = MT.t 
-
-module type EtatSetMake = functor (ET : EtatalType) -> 
-	EtSet with type x = ET.t 
-
-module type AutMake = functor
-	(A : MonSet)
-	(Q : EtSet)
-	(D : EtSet with type x = Q.x)
-	(F : EtSet with type x = Q.x)
-	(T : Set.S with type elt = Q.x * A.x * Q.x) -> 
-	Aut with type a = A.x and type q = Q.x  and type auta = A.t and type autq = Q.t and type autt = T.t
-
-module MonSetMake (MT : MonoidalType) : MonSet with type x = MT.t 
-
-module EtatSetMake (ET : EtatalType) : EtSet with type x = ET.t
-
 module AutMake 
-	(A : MonSet)
-	(Q : EtSet)
-	(D : EtSet with type x = Q.x)
-	(F : EtSet with type x = Q.x)
-	(T : Set.S with type elt = Q.x * A.x * Q.x) :
-	Aut with type a = A.x and type q = Q.x  and type auta = A.t and type autq = Q.t and type autt = T.t 
+	(LS : SetLang)
+	(Q : SetEtat)
+	(T : Set.S with type elt = Q.S.elt * LS.S.elt * Q.S.elt) :
+	Aut with type a = LS.S.elt and type q = Q.S.elt and type auta = LS.S.t and type autq = Q.S.t and type autt = T.t
+
