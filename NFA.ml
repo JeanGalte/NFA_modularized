ae9ada0d602cbdb6fc4981d9e6f93822e34b96e6
JeanGalte <102584320+JeanGalte@users.noreply.github.com>
2023-05-09 20:13:51 +0200

    Update NFA.ml

--- NFA.ml
+++ NFA.ml
@@ -30,9 +30,10 @@ sig
 	module Sub : Monset
 	type t =
 		| B of Sub.S.t
-		| Star of Sub.t
+		| Star of t
 		| Union of t * t
 		| Prod of t * t
+		| Empty
 	val compare : t -> t -> int
 end
 
@@ -41,9 +42,10 @@ struct
 	module Sub = M
 	type t =
 		| B of Sub.S.t
-		| Star of Sub.t
+		| Star of t
 		| Union of t * t
 		| Prod of t * t
+		| Empty
 	let compare = compare
 end
 
@@ -108,6 +110,9 @@ sig
 	val is_auto_from_lists : a list -> q list -> q list -> q list -> (q * a * q) list -> bool  
 	val debuts_fins_uniques : aut -> aut 
 	val est_usuel : aut -> bool
+	val lang_trans : aut -> q -> q -> a 
+	val elimine_etat : aut -> q -> aut
+	val langage_accepte : aut -> a
 end
 
 module AutMake 
@@ -200,4 +205,92 @@ struct
 				| B ens -> LS.Sub.S.cardinal ens = 1
 				| _ -> false
 			) a.alph
+
+	let lang_trans (a : aut) (e1 : q) (e2 : q) : a =
+		let l = T.filter (fun (h,_,j) -> h = e1 && j = e2 ) a.trans in
+		if l = T.empty then 
+			LS.Empty
+		else
+			if T.cardinal l > 1 then 
+				let () = print_string "Attention : les transitions sont étiquettées par des langages entiers dont vous pouvez faire l'union, veillez à ne pas avoir 2 transitions (e1,l1,e2) et (e1,l2,e2) mais plutôt (e1,Union l1 l2,e2). Pour plus d'information, lisez la documentation sur le readme.md"  in LS.Empty
+			else 
+				(fun (_,lang,_) -> lang) (T.choose l)
+
+	let elimine_etat (a : aut) (e : q) : aut = 
+		if Q.S.mem e a.debuts then let () = print_string "On ne peut pas supprimer un état de début de l'automate" in a else
+		if Q.S.mem e a.fins then let () = print_string "On ne peut pas supprimer un état de fin de l'automate" in a else
+		let e_dep = Q.S.filter (fun elt -> T.exists (fun (h,_,_) -> h = e) a.trans) a.etats in
+		let e_fin = Q.S.filter (fun elt -> T.exists (fun (_,_,j) -> j = e) a.trans) a.etats in 
+		(* Produit cartésien du pauvre. La complexité est mauvaise, mais de toute manière dans l'idéal il faudrait faire un foncteur produit cartésien en entier et l'exploiter ici*)
+		let rec compact (l : 'a list list) : 'a list = 
+			match l with
+			| [] -> []
+			| x :: xs -> x @ compact xs
+		in
+		let rec map_all (l : 'b list) (el : 'c)  : ('c * 'b) list =
+			match l with
+			| [] -> []
+			| y :: ys -> (el, y) :: map_all ys el 
+		in 
+		let rec make_cpl (l1 : 'd list) (l2 : 'e list) : ('d * 'e) list list =
+			match l1 with
+			| [] -> []
+			| x :: xs -> (map_all l2 x) :: (make_cpl xs l2) 
+		in
+		let cpls = compact (make_cpl (Q.S.elements e_fin) (Q.S.elements e_dep))
+		in
+		let rec ntrans_list (cpl : (Q.S.elt * Q.S.elt) list) : (T.elt list) = 
+			match cpl with
+			| [] -> []
+			| (e1, e2) :: xs -> 
+				(
+					e1, 
+					LS.Union
+						( 
+							(lang_trans a e1 e2), 
+							(LS.Prod
+								(
+									(lang_trans a e1 e),
+									(LS.Prod 
+										(
+										(LS.Star (lang_trans a e e)), 
+										lang_trans a e e2
+										)
+									)
+								)
+							)
+						)
+					,
+					e2
+				)  
+				:: (ntrans_list xs)
+		in 
+		let n_trans =
+			T.union
+				(T.filter (fun (h,_,j) -> h <> e && j <> e ) a.trans)  
+				(T.of_list (ntrans_list cpls))
+		in
+		let n_alph =
+			LS.S.union		 
+				(LS.S.of_list (List.map (fun (_,l,_) -> l) (T.elements n_trans))) 
+				(a.alph)
+		in 
+		let n_etats = Q.S.filter (fun elt -> elt <> e) a.etats
+		in
+		{
+		alph = n_alph;
+		etats = n_etats;
+		debuts = a.debuts;
+		fins = a.fins;
+		trans = n_trans;
+		}
+
+		let langage_accepte (a : aut) : a = 
+			let etats_elem_list = Q.S.elements a.etats in
+			let na = debuts_fins_uniques a in
+			let rec elimine_success (a : aut) (l : q list) : aut = 
+				match l with
+				| [] -> a
+				| x :: xs -> elimine_success (elimine_etat a x) xs
+			in lang_trans (elimine_success na etats_elem_list) Q.d Q.f 
 end
